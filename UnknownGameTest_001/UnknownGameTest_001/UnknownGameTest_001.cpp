0643a854e712f41f677266949240d2d8b7c4a3bb
DracoNick0 <52973610+DracoNick0@users.noreply.github.com>
2019-09-10 16:56:36 -0700

    Add files via upload

---
+++ UnknownGameTest_001/UnknownGameTest_001/UnknownGameTest_001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string txt;
+	getline(cin, txt);
+	int i = 0;
+	while (txt[i])
+	{
+
+		if (isspace(txt[i]))
+		{
+
+		}
+
+		
+		i++;
+	}
+}
